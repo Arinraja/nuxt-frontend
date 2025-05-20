133e85039b3bc355a7ab7aab97130787323d8e71
Arin raja <rajansar571@gmail.com>
2025-05-21 00:32:55 +0530

    Update createjob.vue

--- pages/createjob.vue
+++ pages/createjob.vue
@@ -74,3 +74,79 @@ async function submitForm() {
   }
 }
 </script>
+
+<template>
+    <div class="py-10 px-6">
+        <h1 class="mb-6 text-2xl">Create job</h1>
+
+        <form v-on:submit.prevent="submitForm" class="space-y-4">
+            <div>
+                <label>Category</label>
+
+                <select v-model="category" class="w-full mt-2 p-4 rounded-xl bg-gray-100">
+                    <option value="">Select category</option>
+                    <option
+                        v-for="category in jobCategories"
+                        v-bind:key="category.id"
+                        v-bind:value="category.id"
+                    >
+                        {{ category.title }}
+                    </option>
+                </select>
+            </div>
+
+            <div>
+                <label>Title</label>
+                <input v-model="title" type="text" class="w-full mt-2 p-4 rounded-xl bg-gray-100">
+            </div>
+
+            <div>
+                <label>Description</label>
+                <textarea v-model="description" class="w-full mt-2 p-4 rounded-xl bg-gray-100"></textarea>
+            </div>
+
+            <div>
+                <label>Salary</label>
+                <input type="text" v-model="position_salary" class="w-full mt-2 p-4 rounded-xl bg-gray-100">
+            </div>
+
+            <div>
+                <label>Location</label>
+                <input type="text" v-model="position_location" class="w-full mt-2 p-4 rounded-xl bg-gray-100">
+            </div>
+
+            <div>
+                <label>Company name</label>
+                <input type="text" v-model="company_name" class="w-full mt-2 p-4 rounded-xl bg-gray-100">
+            </div>
+
+            <div>
+                <label>Company location</label>
+                <input type="text" v-model="company_location" class="w-full mt-2 p-4 rounded-xl bg-gray-100">
+            </div>
+
+            <div>
+                <label>Company e-mail</label>
+                <input type="text" v-model="company_email" class="w-full mt-2 p-4 rounded-xl bg-gray-100">
+            </div>
+
+            <div
+                v-if="errors.length" 
+                class="mb-6 py-4 px-6 bg-rose-400 text-white rounded-xl"
+            >
+                <p v-for="error in errors" v-bind:key="error">
+                    {{ error }}
+                </p>
+            </div>
+
+            <!-- <button class="py-4 px-6 bg-teal-700 text-white rounded-xl">Submit</button> -->
+             <button
+  class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+  style="box-shadow: 0 0 20px 5px #0f5e57;"
+>
+  Submit
+</button>
+
+        </form>
+    </div>
+</template>
