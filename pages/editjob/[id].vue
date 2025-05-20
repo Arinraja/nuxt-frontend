48576d5eec478b0be4b729431388b12e41abe844
Arin raja <rajansar571@gmail.com>
2025-05-21 00:18:50 +0530

    Update [id].vue

--- pages/editjob/[id].vue
+++ pages/editjob/[id].vue
@@ -1,6 +1,7 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useUserStore } from '@/stores/user'
+import { useRoute, useRouter } from 'vue-router'
 
 const userStore = useUserStore()
 const router = useRouter()
@@ -12,7 +13,8 @@ onMounted(() => {
     }
 })
 
-const {data: job} = await useFetch('import.meta.env.VITE_API_URL/jobs/' + route.params.id + '/')
+// Correctly use template literal for environment variable
+const { data: job } = await useFetch(`${import.meta.env.VITE_API_URL}/jobs/${route.params.id}/`)
 
 let title = ref(job.value.title)
 let description = ref(job.value.description)
@@ -28,16 +30,16 @@ async function submitForm() {
 
     errors.value = []
 
-    if (title.value == '') { errors.value.push('The title field is missing')}
-    if (description.value == '') { errors.value.push('The description field is missing')}
-    if (position_salary.value == '') { errors.value.push('The position salary field is missing')}
-    if (position_location.value == '') { errors.value.push('The position location field is missing')}
-    if (company_name.value == '') { errors.value.push('The company name field is missing')}
-    if (company_location.value == '') { errors.value.push('The company location field is missing')}
-    if (company_email.value == '') { errors.value.push('The company email field is missing')}
+    if (title.value == '') errors.value.push('The title field is missing')
+    if (description.value == '') errors.value.push('The description field is missing')
+    if (position_salary.value == '') errors.value.push('The position salary field is missing')
+    if (position_location.value == '') errors.value.push('The position location field is missing')
+    if (company_name.value == '') errors.value.push('The company name field is missing')
+    if (company_location.value == '') errors.value.push('The company location field is missing')
+    if (company_email.value == '') errors.value.push('The company email field is missing')
 
-    if (errors.value.length == 0) {
-        await $fetch('import.meta.env.VITE_API_URL/jobs/' + route.params.id + '/edit/', {
+    if (errors.value.length === 0) {
+        await $fetch(`${import.meta.env.VITE_API_URL}/jobs/${route.params.id}/edit/`, {
             method: 'PUT',
             headers: {
                 'Authorization': 'token ' + userStore.user.token,
@@ -56,8 +58,7 @@ async function submitForm() {
         })
         .then(response => {
             console.log('response', response)
-
-            router.push({path: '/myjobs'})
+            router.push({ path: '/myjobs' })
         })
         .catch(error => {
             if (error.response) {
@@ -67,13 +68,13 @@ async function submitForm() {
                 console.log(JSON.stringify(error.response))
             } else if (error.message) {
                 errors.value.push('Something went wrong. Please try again')
-                
                 console.log(JSON.stringify(error))
             }
         })
     }
 }
 </script>
+
 
 <template>
     <div class="py-10 px-6">
