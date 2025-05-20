7354decb3df76103b11ad6b8f461e9c4571ea7a0
Arin raja <rajansar571@gmail.com>
2025-05-21 00:22:49 +0530

    Update signup.vue

--- pages/signup.vue
+++ pages/signup.vue
@@ -42,3 +42,34 @@ async function submitForm() {
     }
 }
 </script>
+
+<template>
+    <div class="py-10 px-6">
+        <div class="max-w-sm mx-auto py-10 px-6 bg-gray-100 rounded-xl">
+            <h1 class="mb-6 text-2xl">Sign up</h1>
+
+            <form v-on:submit.prevent="submitForm">
+                <input v-model="email" type="email" placeholder="Your email address..." class="w-full mb-4 py-4 px-6 rounded-xl">
+                <input v-model="password1" type="password" placeholder="Your password..." class="w-full mb-4 py-4 px-6 rounded-xl">
+                <input v-model="password2" type="password" placeholder="Repeat password..." class="w-full mb-4 py-4 px-6 rounded-xl">
+
+                <div
+                    v-if="errors.length" 
+                    class="mb-6 py-4 px-6 bg-rose-400 text-white rounded-xl"
+                >
+                    <p v-for="error in errors" v-bind:key="error">
+                        {{ error }}
+                    </p>
+                </div>
+
+                <!-- <button class="py-4 px-6 bg-teal-700 text-white rounded-xl">Submit</button> -->
+                 <button
+                  class="py-4 px-6 bg-teal-700 text-white rounded-xl shadow-lg transition-all duration-300 transform hover:scale-105 ring-2 ring-white ring-opacity-30"
+                  style="box-shadow: 0 0 6px rgba(0,255,150,0.5), 0 0 12px rgba(0,255,150,0.4), 0 0 18px rgba(0,255,150,0.3);"
+                >
+                  Submit
+                </button>
+            </form>
+        </div>
+    </div>
+</template>
