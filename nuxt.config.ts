50afa3458d35372a743866fb6b5e16beed6623d2
Arin raja <rajansar571@gmail.com>
2025-05-21 18:07:58 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
  // devtools: { enabled: true }
   runtimeConfig: {
     public: {
-      apiBase: process.env.VITE_API_URL || 'http://localhost:8000',
+      apiBase: 'https://django-backend-f9hr.onrender.com',
     },
   },
  css: ['~/assets/css/main.css'],
