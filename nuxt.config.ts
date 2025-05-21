3c7c57092e532a261148921d7846f61ac73807a4
Arin raja <rajansar571@gmail.com>
2025-05-21 18:01:44 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,25 @@
-// // https://nuxt.com/docs/api/configuration/nuxt-config
-// export default defineNuxtConfig({
-//  // compatibilityDate: '2024-11-01',
-//  // devtools: { enabled: true }
-//  css: ['~/assets/css/main.css'],
-
-//  postcss: {
-//    plugins: {
-//       tailwindcss: {},
-//       autoprefixer: {},
-//    },
-//  },
-
-//  // compatibilityDate: '2024-12-22',
-//  modules: [
-//    '@pinia/nuxt',
-//  ],
-
-//  compatibilityDate: '2024-12-23',
-// })
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/main.css'],
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-
-  modules: [
-    '@pinia/nuxt',
-  ],
-
+ // compatibilityDate: '2024-11-01',
+ // devtools: { enabled: true }
   runtimeConfig: {
     public: {
-      apiBase: process.env.VITE_API_URL
-    }
+      apiBase: process.env.VITE_API_URL || 'http://localhost:8000',
+    },
   },
+ css: ['~/assets/css/main.css'],
 
-  compatibilityDate: '2024-12-23',
+ postcss: {
+   plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+   },
+ },
+
+ // compatibilityDate: '2024-12-22',
+ modules: [
+   '@pinia/nuxt',
+ ],
+
+ compatibilityDate: '2024-12-23',
 })
