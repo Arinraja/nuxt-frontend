2d471cb8dde9e1bf1dc6b77243fee50da7c17473
Arin raja <rajansar571@gmail.com>
2025-05-21 17:59:20 +0530

    Update myjobs.vue

--- pages/myjobs.vue
+++ pages/myjobs.vue
@@ -1,10 +1,9 @@
-const config = useRuntimeConfig()
-
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 
+const config = useRuntimeConfig()
 const userStore = useUserStore()
 const router = useRouter()
 let jobs = ref([])
@@ -23,20 +22,7 @@ useSeoMeta({
   description: 'The description'
 })
 
-// async function getJobs() {
-//   try {
-//     const response = await $fetch(`${import.meta.env.VITE_API_URL}/jobs/my`, {
-//       headers: {
-//         'Authorization': 'token ' + userStore.user.token,
-//         'Content-Type': 'application/json'
-//       },
-//     })
-//     jobs.value = response
-//   } catch (error) {
-//     console.log('error', error)
-//   }
-// }
-  async function getJobs() {
+async function getJobs() {
   try {
     const response = await $fetch(`${config.public.apiBase}/jobs/my`, {
       headers: {
@@ -46,10 +32,9 @@ useSeoMeta({
     })
     jobs.value = response
   } catch (error) {
-    console.log('error', error)
+    console.error('Error fetching jobs:', error)
   }
 }
-
 
 function deleteJob(id) {
   jobs.value = jobs.value.filter(job => job.id !== id)
@@ -57,32 +42,30 @@ function deleteJob(id) {
 </script>
 
 <template>
-    <div class="py-10 px-6">
-        <!-- <h1 class="mb-6 text-2xl">My jobs</h1> -->
-         
-        <h1
-  class="mb-6 text-2xl text-white px-6 py-3 rounded-lg max-w-max"
-  style="
-    background: linear-gradient(
-      to right,
-      #b22222 0%,
-      #b22222 20%,
-      #0F5E57 20%,
-      #0F5E57 100%
-    );
-  "
->
-  My jobs
-</h1>
+  <div class="py-10 px-6">
+    <h1
+      class="mb-6 text-2xl text-white px-6 py-3 rounded-lg max-w-max"
+      style="
+        background: linear-gradient(
+          to right,
+          #b22222 0%,
+          #b22222 20%,
+          #0F5E57 20%,
+          #0F5E57 100%
+        );
+      "
+    >
+      My jobs
+    </h1>
 
-        <div class="space-y-4">
-            <Job
-                v-for="job in jobs"
-                :key="job.id"
-                :job="job" 
-                :my="true"
-                v-on:deleteJob="deleteJob(job.id)"
-            />
-        </div>
+    <div class="space-y-4">
+      <Job
+        v-for="job in jobs"
+        :key="job.id"
+        :job="job"
+        :my="true"
+        v-on:deleteJob="deleteJob(job.id)"
+      />
     </div>
+  </div>
 </template>
